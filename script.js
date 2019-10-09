9020783b5c967ac901b001a043d42cc73a81a2d2
Ben Deltenre <benjdelt@gmail.com>
2019-10-08 19:59:51 -0700

    Added speech recognition

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+window.SpeechRecognition = window.webkitSpeechRecognition || 
+window.SpeechRecognition;
+
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  // Extension starts on page load
   if (changeInfo.status == 'complete' && tab.active) {
-    alert("Hello");
+    // Event listener on command set in manifest.json
+    chrome.commands.onCommand.addListener(function (command) {
+      if (command === "start-speech") {
+        var recognition = new SpeechRecognition();
+        recognition.start();
+        alert("Started");
+        recognition.continuous = true;
+        setTimeout(function() {
+          recognition.stop();
+          alert("Stopped");
+        }, 5000)
+        recognition.onresult = function(event) {
+          alert("Result: " + event.results[0][0].transcript)
+        }
+      }
+  });
   }
 })
