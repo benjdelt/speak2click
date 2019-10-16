c79d1d77151d234f236c7a23244b92148e2df9e9
Ben Deltenre <benjdelt@gmail.com>
2019-10-16 14:28:13 -0700

    Made speech recognition click on text links

--- script.js
+++ script.js
@@ -1,24 +1,71 @@
-window.SpeechRecognition = window.webkitSpeechRecognition || 
-window.SpeechRecognition;
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-  // Extension starts on page load
-  if (changeInfo.status == 'complete' && tab.active) {
-    // Event listener on command set in manifest.json
-    chrome.commands.onCommand.addListener(function (command) {
-      if (command === "start-speech") {
-        var recognition = new SpeechRecognition();
-        recognition.start();
-        alert("Started");
-        recognition.continuous = true;
-        setTimeout(function() {
-          recognition.stop();
-          alert("Stopped");
-        }, 5000)
-        recognition.onresult = function(event) {
-          alert("Result: " + event.results[0][0].transcript)
-        }
-      }
-  });
+let finalTranscript = '';
+
+let recognition = new window.SpeechRecognition();
+recognition.lang = 'en-US';
+recognition.maxAlternatives = 10;
+recognition.continuous = true;
+
+recognition.onresult = (event) => {
+  for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+    let transcript = event.results[i][0].transcript;
+    if (event.results[i].isFinal) {
+      finalTranscript += transcript;
+    }
+  }
+  document.querySelector('.output').innerHTML = finalTranscript;
+
+  // Get all the links and click on the one that matches the recognition result
+
+  const links = document.querySelectorAll('a');
+  console.log(links);
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].innerText.toLowerCase() === finalTranscript.toLowerCase()) {
+      links[i].click();
+    }
+  } 
+}
+
+window.addEventListener('keydown', e => {
+  if (e.key.toLowerCase() === 'l') {
+    finalTranscript = '';
+    recognition.start();
+    console.log("Listening...")
   }
 })
+
+// Event Listeners for debugging
+
+// recognition.onstart = function() {
+//   console.log("start")
+// }
+
+// recognition.onaudiostart = function() {
+//   console.log("audiostart")
+// }
+
+// recognition.onsoundstart = function() {
+//   console.log("soundstart")
+// }
+
+// recognition.onspeechstart = function() {
+//   console.log("speechstart")
+// }
+
+// recognition.onspeechend = function() {
+//   console.log("speechend")
+// }
+
+// recognition.onsoundend = function() {
+//   // recognition.stop();
+//   console.log("soundend")
+// }
+
+// recognition.onaudioend = function() {
+//   console.log("audioend")
+// }
+
+// recognition.onend = function() {
+//   console.log("end")
+// }
